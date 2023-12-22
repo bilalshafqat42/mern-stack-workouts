763806a017b70c9e50d8ee90a14b29789aa9b626
bilalshafqat42 <bilalshafqat42@gmail.com>
2023-12-22 12:45:42 +0400

    mongodb and mongoose setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workoutRoutes");
 
 // express app
 const app = express();
-  
+
 //middleware
 app.use(express.json()); // this middleware require to read routes file in json
 
@@ -18,6 +19,21 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 
 // listening for request
-app.listen(process.env.PORT, () => {
-  console.log(`listening on Port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`listening on Port ${process.env.PORT}`);
+// });
+
+// connection to database
+mongoose
+  .connect(process.env.MONG0_URI)
+  .then(() => {
+    // listening app for request
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Database connected and listening on Port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
